b32e9fad7041cf53d7e524aee3b952a6aa290368
John A Stevenson <john.stevenson@thinkwhere.com>
2016-09-02 15:47:22 +0100

    Added --exe flag to nosetests.

--- bin/shippable_tests.sh
+++ bin/shippable_tests.sh
@@ -4,6 +4,7 @@ exit 0
 
 find ./ -iname "*.pyc" -delete
 xvfb-run nosetests --with-coverage \
+          --exe \
           --cover-erase \
           --cover-package Roadnet \
           --with-xunit --xunit-file=shippable/testresults/nosetests.xml $1 $2
